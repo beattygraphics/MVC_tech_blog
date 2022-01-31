1da1a07b8fcb4314807823b696201106a744bf4e
Erin Beatty <beattygraphics@icloud.com>
2022-01-30 22:23:59 -0500

    refactor for heroku deploy

--- seeds/commentSeed.js
+++ seeds/commentSeed.js
@@ -7,37 +7,37 @@ const commentData = [
     user_id: 4
   },
   {
-    comment_text: "lskjdf;JSDBC;Jbsd;v jub;SFBV;w",
+    comment_text: "Comment 2",
     post_id: 1,
     user_id: 3
   },
   {
-    comment_text: ";ajksbdc;jksBDC;IJb;pijbdPV;IJbwds",
+    comment_text: "heres another comment",
     post_id: 1,
     user_id: 1
   },
   {
-    comment_text: "kjlsKDNCOKJNKKKK",
+    comment_text: "yet another coment",
     post_id: 1,
     user_id: 2
   },
   {
-    comment_text: "WHAT THE FUCK",
+    comment_text: "Blah blah blah",
     post_id: 1,
     user_id: 5
   },
   {
-    comment_text: "THIS SHIT SUCKS",
+    comment_text: "Wow this project was hard",
     post_id: 2,
     user_id: 1
   },
   {
-    comment_text: "WHY DO I FVEEL LIKE IVE BEEN DOING THIS FOR DAYS",
+    comment_text: "WHY DO I FEEL LIKE IVE BEEN DOING THIS FOR DAYS",
     post_id: 2,
     user_id: 4
   },
   {
-    comment_text: "OK",
+    comment_text: "OK, wait  no...ok",
     post_id: 2,
     user_id: 5
   },
